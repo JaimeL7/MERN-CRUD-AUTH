d2215947f5a93c893e9f80a9cbbbc06f28422415
Jaime L <Jaime_1997@hotmail.es>
2025-05-27 21:15:02 -0500

    Avance curso

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -62,7 +62,18 @@ export const logout = (register, res) => {
   return res.sendStatus(200);
 };
 
-export const profile = (req, res) => {
+export const profile = async (req, res) => {
+
+  const userFound = await User.findById(req.user.id);
+  if (!userFound) return res.status(400).json({ message: "User not found" });
+
+  return res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
+    });
   console.log(req.user);
   res.send("profile");
 };
